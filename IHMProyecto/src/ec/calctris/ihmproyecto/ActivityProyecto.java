493ab28c41d57b03b81419f7b4e334d505b75fe1
Andrea Caceres <andreacaceresm@gmail.com>
2013-01-19 23:43:11 -0500

    Optimizando codigo en la creacion de esferas

--- IHMProyecto/src/ec/calctris/ihmproyecto/ActivityProyecto.java
+++ IHMProyecto/src/ec/calctris/ihmproyecto/ActivityProyecto.java
@@ -4,12 +4,17 @@ import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.AutoParallaxBackground;
 import org.andengine.entity.scene.background.ParallaxBackground.ParallaxEntity;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.util.FPSLogger;
-import org.andengine.input.touch.TouchEvent;
+import org.andengine.extension.physics.box2d.PhysicsConnector;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
+import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.input.sensor.acceleration.AccelerationData;
+import org.andengine.input.sensor.acceleration.IAccelerationListener;
 import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
@@ -17,13 +22,19 @@ import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 
-import android.content.Intent;
+import android.hardware.SensorManager;
 
-public class ActivityProyecto extends SimpleBaseGameActivity {
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+
+public class ActivityProyecto extends SimpleBaseGameActivity implements IAccelerationListener {
 
 	//Constantes
 	private static final int CAMERA_WIDTH = 480; //Ancho 480px
     private static final int CAMERA_HEIGHT = 800; //Alto 800px
+    public int arreglo = 8;
 
     //Variables
     private BitmapTextureAtlas mFondo;//Arreglo de fondo
@@ -32,51 +43,63 @@ public class ActivityProyecto extends SimpleBaseGameActivity {
     private BitmapTextureAtlas mNube;
     private ITextureRegion mNubeRegion;
     
-    private BitmapTextureAtlas mBotones;//Arreglo de botones
-    private ITextureRegion mBoton1;//BotonJugar
-    private ITextureRegion mBoton2;//BotonPuntajes
-    private ITextureRegion mBoton3;//BotonAyuda
-
+    private BitmapTextureAtlas mEsferas;
+    private ITextureRegion[] mFondoEsferas = new ITextureRegion[arreglo];
+    
     private Scene mScene;
+    
+    private PhysicsWorld myPhysicsWorld;
+    
+    private int numeroAleatorio = (int)(Math.random()*9+1);//Genera número entre 1 al 9
+    
+    // ============================================================
+    // Method: onCreateEmgineOptions
+    // ============================================================
 	@Override
 	public EngineOptions onCreateEngineOptions() {
 		
 		final Camera mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
-        return new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera);
-        
+        return new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera);      
 	}
 
+	// ============================================================
+    // Method: onCreateResources
+    // ============================================================
 	@Override
 	protected void onCreateResources() {
 		
 		//Obteniendo la carpeta donde estaran las imagenes
-	    BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
-	    
-	    //Para el fondo
-	    this.mFondo = new BitmapTextureAtlas(this.getTextureManager(), 480, 800, TextureOptions.BILINEAR);//Arreglo donde almaceno la imagen
-	    this.mFondoRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mFondo, this, "PantallaInicial.png", 0, 0);
-	    this.mFondo.load();//Cargo la imagen
-	    
+        BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+        
+        //Para el fondo
+        this.mFondo = new BitmapTextureAtlas(this.getTextureManager(), 480, 800, TextureOptions.BILINEAR);//Arreglo donde almaceno la imagen
+        this.mFondoRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mFondo, this, "PantallaGame.png", 0, 0);
+        this.mFondo.load();//Cargo la imagen
+
 	    //Para el fondo con la nube en movimiento
 	    this.mNube = new BitmapTextureAtlas(this.getTextureManager(), 227, 85, TextureOptions.BILINEAR);
 	    this.mNubeRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mNube, this, "Nubes_pequenas.png", 0, 0);
 	    this.mNube.load();
-	    
-	    //Para los botones
-	    this.mBotones = new BitmapTextureAtlas(this.getTextureManager(),148, 135, TextureOptions.BILINEAR);//Arreglo para los botones iniciales
-	    this.mBoton1 = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mBotones, this, "BotonJugar.png", 0, 0);
-	    this.mBoton2 = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mBotones, this, "BotonPuntajes.png", 0, 45);
-	    this.mBoton3 = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mBotones, this, "BotonAyuda.png", 0, 90);
-	    this.mBotones.load();
-		
+        
+        //Para las esferas
+		this.mEsferas = new BitmapTextureAtlas(this.getTextureManager(), 50, 450, TextureOptions.BILINEAR);
+		String ruta[] = {"Esfera1.png", "Esfera2.png", "Esfera3.png", "Esfera4.png", "Esfera5.png", "Esfera6.png", "Esfera7.png", "Esfera8.png", "Esfera9.png"};
+		for(int i = 0; i < arreglo; i++){
+			this.mFondoEsferas[i] = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mEsferas, this, ruta[i], 0, i*50);
+		}
+		this.mEsferas.load();
 	}
 
 	@Override
 	protected Scene onCreateScene() {
+		
 		this.mEngine.registerUpdateHandler(new FPSLogger());
         this.mScene = new Scene();
-        
         final VertexBufferObjectManager vertexBufferObjectManager = this.getVertexBufferObjectManager();
+        this.myPhysicsWorld = new PhysicsWorld(new Vector2(0, SensorManager.GRAVITY_EARTH), false);
+        //El body del mundo fisico
+        final Body[] body = new Body[arreglo];
+        final Sprite[] esf = new Sprite[arreglo];
         
         //Para el fondo
         final AutoParallaxBackground fondo = new AutoParallaxBackground(0, 0, 0, 5);
@@ -84,43 +107,75 @@ public class ActivityProyecto extends SimpleBaseGameActivity {
         fondo.attachParallaxEntity(new ParallaxEntity(-10.0f, new Sprite(0, 0, this.mNubeRegion, vertexBufferObjectManager)));
         mScene.setBackground(fondo);
         
-        //Para los botones
-        //BotonJugar
-        final Sprite boton1 = new Sprite(0, CAMERA_HEIGHT - this.mBoton1.getHeight() - 390, this.mBoton1, vertexBufferObjectManager){
-        	@Override
-			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY){
-        		Intent intent = new Intent (ActivityProyecto.this, PantallaJugar.class);
-        		startActivity(intent);
-        		return true;
-        	}
-        };
-        mScene.registerTouchArea(boton1);//Se registra el evento
-        mScene.attachChild(boton1);
-        //JugarPuntajes
-        final Sprite boton2 = new Sprite(0, CAMERA_HEIGHT - this.mBoton2.getHeight() - 390 + 55, this.mBoton2, vertexBufferObjectManager){
-        	@Override
-			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY){
-        		Intent intent = new Intent (ActivityProyecto.this, PantallaPuntajes.class);
-        		startActivity(intent);
-        		return true;
-        	}
-        };
-        mScene.registerTouchArea(boton2);
-        mScene.attachChild(boton2);
-        //BotonAyuda
-        final Sprite boton3 = new Sprite(0, CAMERA_HEIGHT - this.mBoton3.getHeight() - 390 + 110, this.mBoton3, vertexBufferObjectManager){
-        	@Override
-			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY){
-        		Intent intent = new Intent (ActivityProyecto.this, PantallaAyuda.class);
-        		startActivity(intent);
-        		return true;
-        	}
-        };
-        mScene.registerTouchArea(boton3);
-        mScene.attachChild(boton3);
-                                        
+        //El mundo físico
+        final Rectangle pared1 = new Rectangle(0, 0, 1, CAMERA_HEIGHT, vertexBufferObjectManager);
+        final Rectangle pared2 = new Rectangle(0, CAMERA_HEIGHT, 317, 1, vertexBufferObjectManager);
+        final Rectangle pared3 = new Rectangle(317, 0, 1, CAMERA_HEIGHT, vertexBufferObjectManager);
+        final Rectangle pared4 = new Rectangle(0, 0, CAMERA_WIDTH, 1, vertexBufferObjectManager);
+        
+        //Le doy texture dentro del mundo fisico
+        final FixtureDef texturepared = PhysicsFactory.createFixtureDef(0, 0.1f, 0.5f);
+        PhysicsFactory.createBoxBody(this.myPhysicsWorld,pared1,BodyType.StaticBody,texturepared);
+        PhysicsFactory.createBoxBody(this.myPhysicsWorld,pared2,BodyType.StaticBody,texturepared);
+        PhysicsFactory.createBoxBody(this.myPhysicsWorld,pared3,BodyType.StaticBody,texturepared);
+        PhysicsFactory.createBoxBody(this.myPhysicsWorld,pared4,BodyType.StaticBody,texturepared);
+        
+        //Creando Sprite para cada esfera
+        for (int i = 0; i<arreglo; i++){
+        	esf[i] = new Sprite(158, 0, this.mFondoEsferas[i], vertexBufferObjectManager);
+        }
+        
+        //Creando las esferas en el mundo físico
+        for (int y = 0; y<arreglo; y++){
+        	body[y] = PhysicsFactory.createBoxBody(this.myPhysicsWorld, esf[y], BodyType.DynamicBody, texturepared);
+        }
+        
+        //Para interactuar
+        for (int z = 0; z<arreglo; z++){
+        	this.myPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(esf[z],body[z],true,true));
+        }
+        
+        //Le hago un merge
+        for (int a = 0; a<arreglo; a++){
+        	esf[a].setUserData(body[a]);
+        }
+        
+        //Agrego las esferas al scene
+        //Aqui tengo q darle aleatoridad
+        for (int b = 0; b<arreglo; b++){
+        	mScene.attachChild(esf[b]);	
+        }
+        //mScene.attachChild(esf);
+        mScene.attachChild(pared1);
+        mScene.attachChild(pared2);
+        mScene.attachChild(pared3);
+        mScene.attachChild(pared4);
+        
+        //Se retorna
         this.mScene.setOnSceneTouchListenerBindingOnActionDownEnabled(true);
+        this.mScene.registerUpdateHandler(this.myPhysicsWorld);
         return mScene;
 	}
-    
+	
+	public void OnResumeGame(){
+		super.onResumeGame();
+		this.enableAccelerationSensor(this);
+	}
+
+	@Override
+	public void onAccelerationAccuracyChanged(AccelerationData pAccelerationData) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onAccelerationChanged(AccelerationData pAccelerationData) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void OnPauseGame(){
+		super.onPauseGame();
+		this.disableAccelerationSensor();
+	}
 }
